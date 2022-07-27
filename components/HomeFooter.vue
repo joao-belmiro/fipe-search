770ccf4c5557a04abbf5160470e633b2aba2781e
João Carlos Belmiro Gonçalves <joao.goncalves@compasso.com.br>
2022-07-27 10:31:24 -0300

    criando footer

---
+++ components/HomeFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <p class="copyright">
+      © Seu Fipe -
+      <a href="https://github.com/joao-belmiro" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+      João Carlos - 2022
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'HomeFooter'
+}
+</script>
+
+<style>
+footer {
+    margin-top: 32px;
+    width: 100%;
+    padding: 32px 0;
+    color: #f7fafc;
+    background-color:#1a202c;
+}
+.copyright {
+    text-align: center;
+}
+p a {
+  color: #00dc82;
+  text-decoration: none;
+}
+</style>
